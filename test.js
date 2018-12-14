6313540263237f4c81516a9c530c1b6b13bb6b16
j-d-b <jacob@jdbrady.info>
2018-12-14 14:04:45 -0500

    adding test cases

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 const findDuplicates = require('./index.js');
 
+test('empty array', () => {
+  const arr = [];
+  const dupes = findDuplicates([]);
+  expect(dupes).toHaveLength(0);
+});
+
+test('no duplicates', () => {
+  const arr = [1, 2, 3];
+  const dupes = findDuplicates(arr);
+  expect(dupes).toHaveLength(0);
+});
+
 test('single repetition', () => {
   const arr = ['a', 'b', 'b', 'c'];
   const dupes = findDuplicates(arr);
@@ -7,15 +19,14 @@ test('single repetition', () => {
   expect(dupes).toContain('b');
 });
 
-test('multiple duplicates with mutliple repeats', () => {
-  const arr = [1, 2, 1, 1, 2, 4, 5, 6, 1];
+test('multiple repeats', () => {
+  const arr = [1, 2, 1, 1, 4, 5, 6, 1];
   const dupes = findDuplicates(arr);
-  expect(dupes).toHaveLength(2);
+  expect(dupes).toHaveLength(1);
   expect(dupes).toContain(1);
-  expect(dupes).toContain(2);
 });
 
-test('mixed types; multiple repeats', () => {
+test('multiple duplicates and multiple repeats', () => {
   const arr = [1, 'a', 1, 'a', 1, 'a', 5, 6, 'b'];
   const dupes = findDuplicates(arr);
   expect(dupes).toHaveLength(2);
